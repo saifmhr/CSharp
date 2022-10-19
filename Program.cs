7f554ac20c410b430db9f1f8fba1cc01facd8c5e
saifmhr <saifmahar165@gmail.com>
2022-10-19 19:34:27 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+  class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Main function");
+        forloop();
+        whileloop();
+        }
+
+    static void forloop()
+    {
+        Console.WriteLine("for Loop  ");
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("0 to 9 =" + i);
+        }
+    }
+
+    static void whileloop()
+    {
+        Console.WriteLine("While loop");
+        int j=0;
+        while (j<5)
+        {
+            Console.WriteLine("while loop = " + j);
+            j++;
+        }
+    }
+    }
